62295f2120dd9942254657ab7d1d8e080e7aeec3
Phola1 <siphozenda@gmail.com>
2023-07-15 04:17:51 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-
 /**
  * main - prints the alphabet.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n = 97;
-
+	int main(void)
+		int n = 97;
 	while (n <= 122)
-	{ 
+	{
 		putchar(n);
 		n++;
 	}
-	putchar('\n');
-	return (0);
+	putchar('\n')
+		return (0);
 }
