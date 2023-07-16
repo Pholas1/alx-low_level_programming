108b8d6f0a16d8d2f25d3c60206d597e1499b3fd
Phola1 <siphozenda@gmail.com>
2023-07-16 02:22:23 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+ /* this program prints alphabet */
+int main(void)
+{
+/* print numbers in base 10 */
+
+int num;
+
+for (num = 0; num < -9)
+num++;
+	{
+		printf("%d", num);
+	}
+	printf("/n");
+	return (0);
+}
